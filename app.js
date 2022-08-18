91cc1d629931f7c719d0844454047eced2a706b5
viniciusdammit <rodriguesvinicius01@hotmail.com>
2022-08-18 10:32:33 -0300

    adicionado novo log

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("rodando sistema de gerenciamento de jogos")
 console.log("Aplicação esta se conectando ao bando de dados")
+    // mais um console log de teste:
+console.log("o Paulo fez esse commit sem você ver na aula!")
